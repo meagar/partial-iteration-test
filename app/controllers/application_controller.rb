23933afccfac2e1ea82edc6de5ee57614165221b
Matthew Eagar <meagar@gmail.com>
2017-01-24 16:33:13 -0500

    POC for busted partial rendering

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def index
+    a = {}
+    b = {}
+    def a.to_partial_path; 'a'; end
+    def b.to_partial_path; 'b'; end
+
+    @het_collection = [a, b]
+    @hom_collection = [a, a]
+  end
 end
